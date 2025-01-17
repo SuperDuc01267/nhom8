0f76a53431d1e43851b4e9e2bc70afc0e7977d18
SuperDuc01267 <114936289+SuperDuc01267@users.noreply.github.com>
2025-01-17 09:12:49 +0700

    Delete src directory

--- src/api/recipeAPI.js
+++
@@ -1,8 +0,0 @@
-export const getRecipes = async () => {
-  const response = await fetch("http://localhost:5000/recipes"); // Đảm bảo đường dẫn này đúng
-  if (!response.ok) {
-    throw new Error("Error fetching recipes");
-  }
-  const data = await response.json();
-  return data;
-};
